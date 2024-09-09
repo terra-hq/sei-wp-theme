5f7410c93e04effd39b53573ae4d0a1502a26169
Eli <eli@landt.co>
2024-09-09 17:12:39 +0200

    fix: 🐛 Correct dev, stage and prod deploy paths

--- config/sftpConfig.js
+++ config/sftpConfig.js
@@ -1,49 +1,49 @@
 const devSFTPConfig = {
-    host: "example.sftp.wpengine.com",
-    user: "example-terra",
-    port: "2222",
-    pass: "9dh393havz",
-    remotePath: "/wp-content/themes/your-theme"
-  };
-  
-  const stageSFTPConfig = {
-    host: "example.sftp.wpengine.com",
-    user: "example-terra",
-    port: "2222",
-    pass: "y$3@@&Wd",
-    remotePath: "/wp-content/themes/your-theme"
-  };
-  
-  const prodSFTPConfig = {
-    host: "example.sftp.wpengine.com",
-    user: "example-terra",
-    port: "2222",
-    pass: "hqszdHi6Y#",
-    remotePath: "/wp-content/themes/your-theme"
-  };
+  host: "seidevelopment.sftp.wpengine.com",
+  user: "seidevelopment-dev",
+  port: "2222",
+  pass: "n75Bqvk5JlSb",
+  remotePath: "/wp-content/themes/sei-wp-vite",
+};
 
-  const filesToExclude = [
-    "!functions/project/hash.php",
-    "!functions/project/local-variable.php",
-    "!public/**/*", // Exclude public folder and everything inside
-    "!config/**/*", // Exclude config folder and everything inside
-    "!node_modules/**/*", // Exclude node_modules folder and everything inside
-    "!src/**/*", // Exclude src folder and everything inside
-    "!.env.production", // Exclude .env.production file
-    "!.env.virtual", // Exclude .env.virtual file
-    "!gulpfile.js", // Exclude gulpfile.js
-    "!package-lock.json", // Exclude package-lock.json file
-    "!package.json", // Exclude package.json file
-    "!readme.md", // Exclude readme.md file
-    "!vite.config.js", // Exclude vite.config.js file
-    "!documentation/**/*", // Exclude documentation folder and everything inside
-    "!*.zip", // Exclude all zip files
-    "!*.tgz", // Exclude all tgz files
-  ];
-  
-  module.exports = {
-    devSFTPConfig,
-    stageSFTPConfig,
-    prodSFTPConfig,
-    filesToExclude
-  };
+const stageSFTPConfig = {
+  host: "stagesei.sftp.wpengine.com",
+  user: "stagesei-stage",
+  port: "2222",
+  pass: "9s0GQkFyvEb1",
+  remotePath: "/wp-content/themes/sei-wp-vite",
+};
+
+const prodSFTPConfig = {
+  host: "seiconsulting.sftp.wpengine.com",
+  user: "seiconsulting-terra",
+  port: "2222",
+  pass: ",q$6!.nf63e0t;^^U",
+  remotePath: "/wp-content/themes/sei-wp-vite",
+};
+
+const filesToExclude = [
+  "!functions/project/hash.php",
+  "!functions/project/local-variable.php",
+  "!public/**/*", // Exclude public folder and everything inside
+  "!config/**/*", // Exclude config folder and everything inside
+  "!node_modules/**/*", // Exclude node_modules folder and everything inside
+  "!src/**/*", // Exclude src folder and everything inside
+  "!.env.production", // Exclude .env.production file
+  "!.env.virtual", // Exclude .env.virtual file
+  "!gulpfile.js", // Exclude gulpfile.js
+  "!package-lock.json", // Exclude package-lock.json file
+  "!package.json", // Exclude package.json file
+  "!readme.md", // Exclude readme.md file
+  "!vite.config.js", // Exclude vite.config.js file
+  "!documentation/**/*", // Exclude documentation folder and everything inside
+  "!*.zip", // Exclude all zip files
+  "!*.tgz", // Exclude all tgz files
+];
+
+module.exports = {
+  devSFTPConfig,
+  stageSFTPConfig,
+  prodSFTPConfig,
+  filesToExclude,
+};
