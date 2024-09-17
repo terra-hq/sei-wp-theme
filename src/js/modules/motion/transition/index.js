5011072a4d881277cff5fa93c7cc0b7d8935a741
Eli <eli@landt.co>
2024-09-17 11:26:48 +0200

    refactor: 🔄 Move smoothScrollToTop to out inside transition

--- src/js/modules/motion/transition/index.js
+++ src/js/modules/motion/transition/index.js
@@ -2,6 +2,7 @@ import gsap from "gsap";
 import In from "./In";
 import Out from "./Out";
 import { hideDropdown, hideSidenav, smoothScrollToTop } from "./utilities";
+import { u_take_your_time } from '@andresclua/jsutil';
 
 export const createTransitionOptions = (payload) => {
   return [
@@ -77,15 +78,29 @@ export const createTransitionOptions = (payload) => {
         if (document.querySelector(".c--hero-b")) {
           tl.add(new window["animations"]["heroB"].default(), "-=.3");
         }
-
-        if (payload.forceScroll) {
-          await smoothScrollToTop();
-        }
+        
       },
 
       out: (next, infos) => {
         var tl = gsap.timeline({
-          onComplete: next,
+          onComplete: async () => {
+            if (window.scrollY !== 0) {
+              /**
+               *  We add this last step in order to prevent animations being executed while the page is scrolling to top
+               *  This is a common issue when the user clicks on a link and the page is scrolled to top
+               *  NOTE : This is the first project we do this, so we need to test it in other projects, and could be a good reference 
+               *  for the rest of the team, last but not least we add ad {u_take_your_time} to gain a few milliseconds before the next page is loaded
+               * !you can change the value of forceScroll in Main.js
+               */  
+              if(payload.forceScroll){
+                await smoothScrollToTop();
+              }
+            }
+        
+            await u_take_your_time(150);
+        
+            next();
+          }
         });
 
         // Hide Dropdown and Sidenav if active
