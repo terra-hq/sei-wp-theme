41d208341bbb99882cb22fdcd442323da731a1a0
Eli <eli@landt.co>
2024-09-25 10:55:17 +0200

    feat: 🚀 Update terraLighthouse mails

--- functions/project/terraLighthouse.php
+++ functions/project/terraLighthouse.php
@@ -2,7 +2,7 @@
 
 // Create an instance of the TerraLighthouse class with specified parameters
 new TerraLighthouse((object) array(
-    'email' => array("eli@terrahq.com"),  // Email address to be used in the class
+    'email' => array("eli@terrahq.com", "maria@terrahq.com", "andres@terrahq.com", "nerea@terrahq.com"),  // Email address to be used in the class
     'interval' => 43200,                // Interval (in seconds) for some functionality in the class (every 12 hours)
     'url' => 'https://www.sei.com/', // !URL to be used in the class, change this accordingly please add wwww always to the URL
 ));
