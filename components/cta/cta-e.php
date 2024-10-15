4e3bbd2080f2ff8b9e75b4bb77f90ff3216019e2
Eli <eli@landt.co>
2024-10-15 14:30:13 +0200

    fix: 🐛 CTA e bg image Ticket: <https://app.clickup.com/t/86duxfdwa>

--- components/cta/cta-e.php
+++ components/cta/cta-e.php
@@ -1,6 +1,6 @@
 <?php if ($module['cta_link']) : ?>
     <a href="<?php echo $module['cta_link']['url'] ?>" class="c--cta-e">
-        <img data-src="<?php bloginfo('template_url'); ?>img/bg/cta-e-bg-02.webp" 
+        <img data-src="<?php bloginfo('template_url'); ?>/img/bg/cta-e-bg-02.webp" 
             class="c--cta-e__bg-items g--lazy-01" 
         >
         <div class="c--cta-e__wrapper">
