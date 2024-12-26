30b1d331da70928b756803f57c11a15678dce160
Eli <eli@landt.co>
2024-12-26 14:13:18 +0100

    feat: 🚀 new RFI page Ticket: <https://app.clickup.com/t/86dvh4pmr>

---
+++ page-rfi.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Template Name: RFI
+*/
+?>
+<?php get_header(); ?>
+
+
+<?php
+$hero_title = get_field('hero_title');
+$hero_subtitle = get_field('hero_subtitle');
+$hero_form_id = get_field('hero_form_id');
+$hero_form_portal = get_field('hero_form_portal');
+include (locate_template('components/hero/hero-h.php', false, false));
+?>
+
+
+<?php get_footer(); ?>
