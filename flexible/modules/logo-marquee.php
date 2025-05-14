4c8ac575e16222396d7d1843edac81ae3c1aa17d
aresmiros <ares@terrahq.com>
2025-05-14 16:34:10 +0200

    feat: 🚀 marquee

---
+++ flexible/modules/logo-marquee.php
@@ -0,0 +1,32 @@
+<section class="<?= $spacing ?>">
+    <div class="f--container">
+        <div class="f--row">
+            
+                <div class="c--marquee-b js--marquee" data-speed=1 data-controls-on-hover="false" data-reversed="true">
+                    <div class="c--marquee-b__list-group " >
+                        <img data-src="<?php bloginfo('template_url'); ?>img/logos/sei-logo.svg"
+                            class="c--cta-e__bg-items g--lazy-01">
+
+                    </div>
+                    <div class="c--marquee-b__list-group " >
+                        <img data-src="<?php bloginfo('template_url'); ?>img/logos/sei-logo.svg"
+                            class="c--cta-e__bg-items g--lazy-01">
+
+                    </div>
+                    <div class="c--marquee-b__list-group " >
+                        <img data-src="<?php bloginfo('template_url'); ?>img/logos/sei-logo.svg"
+                            class="c--cta-e__bg-items g--lazy-01">
+
+                    </div>
+                    <div class="c--marquee-b__list-group " >
+                        <img data-src="<?php bloginfo('template_url'); ?>img/logos/sei-logo.svg"
+                            class="c--cta-e__bg-items g--lazy-01">
+
+                    </div>
+                </div>
+
+            
+
+        </div>
+    </div>
+</section>
---
+++ page-ares.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Template Name: Ares
+*/
+?>
+<?php get_header(); ?>
+
+<section class="f--pt-20 f--pb-20 <?= $spacing ?>">
+    <div class="f--container">
+        <div class="f--row">
+          
+            <div class="c--marquee-a js--marquee " data-speed="1" data-controls-on-hover="false" data-reversed="true">
+                <img width="83" height="32" src="<?= get_template_directory_uri() . "/img/logos/sei-logo.svg" ?>" alt="SEI logo" class="c--marquee-a__item" decoding="async">
+                <img width="83" height="32" src="<?= get_template_directory_uri() . "/img/logos/sei-logo.svg" ?>" alt="SEI logo" class="c--marquee-a__item" decoding="async">
+                <img width="83" height="32" src="<?= get_template_directory_uri() . "/img/logos/sei-logo.svg" ?>" alt="SEI logo" class="c--marquee-a__item" decoding="async">
+                <img width="83" height="32" src="<?= get_template_directory_uri() . "/img/logos/sei-logo.svg" ?>" alt="SEI logo" class="c--marquee-a__item" decoding="async">
+                <img width="83" height="32" src="<?= get_template_directory_uri() . "/img/logos/sei-logo.svg" ?>" alt="SEI logo" class="c--marquee-a__item" decoding="async">
+                <img width="83" height="32" src="<?= get_template_directory_uri() . "/img/logos/sei-logo.svg" ?>" alt="SEI logo" class="c--marquee-a__item" decoding="async">
+    
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<?php get_footer(); ?>
