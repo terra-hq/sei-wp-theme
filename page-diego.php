2a7bb8894f65b622750e4f9ac4587757162563b4
Diego Viador <160519480+diegoviador@users.noreply.github.com>
2025-05-14 17:37:07 +0200

    feat: 🚀 Add Module: Divider frontend
    Ticket: <https://app.clickup.com/t/86dwrqw3x>

---
+++ page-diego.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Template Name: Ares
+*/
+?>
+<?php get_header(); ?>
+
+
+<!-- Divider - spacing must be dynamic -->
+<section class="">
+    <div class="f--container--fluid">
+        <div class="f--row">
+            <div class="f--col-12">
+                <hr class="c--divider-a c--divider-a--third">
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<?php get_footer(); ?>
