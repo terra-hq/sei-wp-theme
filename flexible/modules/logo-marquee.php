53d2f97bc36067339222e9b639391febe079e992
Nerea Fontecha <nerea@landt.co>
2025-05-15 11:49:47 +0200

    logo marquee

--- flexible/modules/logo-marquee.php
+++ flexible/modules/logo-marquee.php
@@ -8,10 +8,9 @@
     <div class="f--container">
         <div class="f--row">
             <div class="f--col-12">
-              
                 <div class="c--marquee-a js--marquee f--col-12  <?= $direction ?> " data-speed="1" data-controls-on-hover="false" data-reversed="false">
-                    <?php foreach($logos as $logo): ?>
-                            <img class="c--marquee-a__item" src="<?= $logo['url'] ?>" alt="<?= $logo['alt'] ?>" />
+                    <?php foreach($logos as $key=> $logo): ?>
+                        <img class="c--marquee-a__item <?= $key == 0 ? 'c--marquee-a__item--initial' : ''?>" src="<?= $logo['url'] ?>" alt="<?= $logo['alt'] ?>" />
                     <?php endforeach; ?>
                 </div>
             </div>
