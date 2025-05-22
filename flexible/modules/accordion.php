cedfd64d1ad03d9095fbd63195ab4739324650df
Nerea Fontecha <nerea@landt.co>
2025-05-22 12:32:37 +0200

    acc name

--- flexible/modules/accordion.php
+++ flexible/modules/accordion.php
@@ -6,7 +6,7 @@
             <div class="f--col-8 f--col-tabletm-10 f--col-tablets-12">
                 <?php foreach($module['accordion_items'] as $key => $accItem): ?>
                 <div class="g--accordion-02 <?= $modifier ?> js--accordion-02">
-                    <button class="g--accordion-02__hd" type="button" data-accordion02-control="simpleContent02-0<?= $key?>-<?= $keyIndexModule ?>" aria-expanded="false">
+                    <button class="g--accordion-02__hd" type="button" data-accordion02-control="acc-0<?= $key?>-<?= $keyIndexModule ?>" aria-expanded="false">
                         <span class="g--accordion-02__hd__item-primary"><?= $accItem['accordion_title']?></span>
                         <span class="g--accordion-02__hd__icon" role="presentation">
                             <svg width="16" height="15" viewBox="0 0 16 15" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -15,7 +15,7 @@
                             </svg>
                         </span>
                     </button>
-                    <div class="g--accordion-02__bd" data-accordion02-content="simpleContent02-0<?= $key?>-<?= $keyIndexModule ?>" aria-hidden="true">
+                    <div class="g--accordion-02__bd" data-accordion02-content="acc-0<?= $key?>-<?= $keyIndexModule ?>" aria-hidden="true">
                         <div class="g--accordion-02__bd__content">
                             <?= $accItem['accordion_title_copy']?>
                         </div>
