1bb0bb36b6a5bf2669e9cd0863eb8e5d80a2230e
mery-gs <maria.gs@terrahq.com>
2025-05-22 16:36:04 +0200

    fixing card 05 styles

--- components/card/card-05.php
+++ components/card/card-05.php
@@ -1,25 +1,34 @@
-<div class="u--display-flex f--col-4 f--col-tabletm-6 f--col-mobile-12">
-    <a href="#" target="_blank" rel="noopener noreferrer" class="g--card-05">
-        <div class="g--card-05__ft-items">
-            <h3 class="g--card-05__ft-items__item-primary">This is the title lorem ipsum dolor sit amet consecteur elit fermentum posuere.</h3>
-            <div class="g--card-05__ft-items__item-secondary c--content-a">
-                <p>This is a short description lorem ipsum dolor sit amet consecteur elit fermentum posuere. Lorem ipsum dolor sit amet consecteur elit fermentum posuere.</p>
-                <ul>
-                    <li>Bullet point text here</li>
-                    <li>Bullet point text here</li>
-                    <li>Bullet point text here</li>
-                </ul>
-            </div>
-            <div class="g--card-05__ft-items__list-group">
-                <p class="g--card-05__ft-items__list-group__item">Free Discovery</p>
-            </div>
-            <figure class="g--card-05__ft-items__media-wrapper">
-                <img width=600 height=700
-                    src=<? url('#{$base-path}/assets/img/atlassian.png'); ?>
-                    alt="atlassian logo"
-                    class="g--card-05__ft-items__media-wrapper__media g--lazy-01"
-                />
-            </figure>
+<!-- Todos los campos de la card pueden ser custom -->
+
+<a href="<?php echo $permalink ?>" <?php echo $target ?> class="g--card-05">
+    <div class="g--card-05__ft-items">
+        <h3 class="g--card-05__ft-items__item-primary">This is the title lorem ipsum dolor sit amet consecteur elit fermentum posuere.</h3>
+        <div class="g--card-05__ft-items__item-secondary c--content-a">
+            <p>This is a short description lorem ipsum dolor sit amet consecteur elit fermentum posuere. Lorem ipsum dolor sit amet consecteur elit fermentum posuere.</p>
+            <ul>
+                <li>Bullet point text here</li>
+                <li>Bullet point text here</li>
+                <li>Bullet point text here</li>
+            </ul>
         </div>
-    </a>
-</div>
+        <div class="g--card-05__ft-items__list-group">
+            <p class="g--card-05__ft-items__list-group__item">Free Discovery</p>
+        </div>
+        <figure class="g--card-05__ft-items__media-wrapper">
+            <?php
+                $image_tag_args = array(
+                    'image' => $image,
+                    'sizes' => '180px',
+                    'class' => 'g--card-05__media-wrapper__media',
+                    'isLazy' => false,
+                    'lazyClass' => 'g--lazy-01',
+                    'showAspectRatio' => true,
+                    'decodingAsync' => true,
+                    'fetchPriority' => false,
+                    'addFigcaption' => false,
+                );
+                generate_image_tag($image_tag_args)
+            ?>
+        </figure>
+    </div>
+</a>
