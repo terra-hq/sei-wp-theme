e02b89941ddd2d0140b86f61c4dbf4113c508b5c
felipeterrahq <felipe@terrahq.com>
2025-05-23 12:23:45 +0200

    feat: 🚀 slider resources new style

--- components/card/card-05.php
+++ components/card/card-05.php
@@ -1,21 +1,35 @@
-<!-- Todos los campos de la card pueden ser custom -->
-
-<a href="<?php echo $permalink ?>" <?php echo $target ?> class="g--card-05">
+<a href="<?= esc_url($permalink); ?>" <?= $target ?> class="g--card-05">
     <div class="g--card-05__ft-items">
-        <h3 class="g--card-05__ft-items__item-primary">This is the title lorem ipsum dolor sit amet consecteur elit fermentum posuere.</h3>
-        <div class="g--card-05__ft-items__item-secondary c--content-a">
-            <p>This is a short description lorem ipsum dolor sit amet consecteur elit fermentum posuere. Lorem ipsum dolor sit amet consecteur elit fermentum posuere.</p>
-            <ul>
-                <li>Bullet point text here</li>
-                <li>Bullet point text here</li>
-                <li>Bullet point text here</li>
-            </ul>
-        </div>
-        <div class="g--card-05__ft-items__list-group">
-            <p class="g--card-05__ft-items__list-group__item">Free Discovery</p>
-        </div>
-        <figure class="g--card-05__ft-items__media-wrapper">
-            <?php
+
+        <?php if ($title): ?>
+            <h3 class="g--card-05__ft-items__item-primary"><?= esc_html($title); ?></h3>
+        <?php endif; ?>
+
+        <?php if ($subtitle || !empty($bullets)): ?>
+            <div class="g--card-05__ft-items__item-secondary c--content-a">
+                <?php if ($subtitle): ?>
+                    <p><?= esc_html($subtitle); ?></p>
+                <?php endif; ?>
+
+                <?php if (!empty($bullets)): ?>
+                    <ul>
+                        <?php foreach ($bullets as $bullet): ?>
+                            <li><?= esc_html($bullet['bullet_text']); ?></li>
+                        <?php endforeach; ?>
+                    </ul>
+                <?php endif; ?>
+            </div>
+        <?php endif; ?>
+
+        <?php if ($label): ?>
+            <div class="g--card-05__ft-items__list-group">
+                <p class="g--card-05__ft-items__list-group__item"><?= esc_html($label); ?></p>
+            </div>
+        <?php endif; ?>
+
+        <?php if ($image): ?>
+            <figure class="g--card-05__ft-items__media-wrapper">
+                <?php
                 $image_tag_args = array(
                     'image' => $image,
                     'sizes' => '180px',
@@ -27,8 +41,10 @@
                     'fetchPriority' => false,
                     'addFigcaption' => false,
                 );
-                generate_image_tag($image_tag_args)
-            ?>
-        </figure>
+                generate_image_tag($image_tag_args);
+                ?>
+            </figure>
+        <?php endif; ?>
+
     </div>
 </a>
--- flexible/modules/slider-resources.php
+++ flexible/modules/slider-resources.php
@@ -1,75 +1,26 @@
 <?php
 $spacing = get_spacing($module['spacing']);
-$how_to_loop = $module['how_to_loop'];
-$pick_topic = $module['pick_topic'];
-$pick_type = $module['pick_type'];
-
-$args = array(
-    'post_type' => 'insight',
-    'posts_per_page' => 9,
-    'orderby' => 'date',
-    'order' => 'DESC',
-);
-
-switch ($how_to_loop) {
-    case 'topic':
-        $args = array(
-            'post_type' => 'insight',
-            'posts_per_page' => 9,
-            'orderby' => 'date',
-            'order' => 'DESC',
-            'tax_query' => array(
-                array(
-                    'taxonomy' => 'topics',
-                    'field'    => 'id',
-                    'terms'    => $pick_topic,
-                ),
-            ),
-        );
-        break;
-
-    case 'type':
-        $args = array(
-            'post_type' => 'insight',
-            'posts_per_page' => 9,
-            'orderby' => 'date',
-            'order' => 'DESC',
-            'tax_query' => array(
-                array(
-                    'taxonomy' => 'insight-types',
-                    'field'    => 'id',
-                    'terms'    => $pick_type,
-                ),
-            ),
-        );
-        break;
-}
 ?>
+
 <section class="c--slider-a <?= $spacing ?>">
     <div class="f--container">
         <div class="f--row">
             <div class="f--col-12">
                 <div class="c--slider-a__wrapper js--slider-c">
-                    <?php
-                    $custom_query = new WP_Query($args);
-                    if ($custom_query->have_posts()) :
-                        while ($custom_query->have_posts()) : $custom_query->the_post();
-                            $insight_types = get_the_terms(get_the_ID(), 'insight-types');
-                            if ($insight_types && !is_wp_error($insight_types)) {
-                                $insight_type = $insight_types[0];
-                                $insight_type_name = $insight_type->name;
-                            }
-                            $title = get_the_title();
-                            $topics = get_the_terms(get_the_ID(), 'topics');
-                            $permalink = get_the_permalink();
-                            $image = get_post_thumbnail_id(get_the_ID());
-                                include(locate_template('components/card/card-05.php', false, false));
-                        endwhile;
-                    endif;
-                    wp_reset_postdata();
-                    ?>
+                    <?php if (!empty($module['custom_cards'])): ?>
+                        <?php foreach ($module['custom_cards'] as $key => $card):
+                            $title = $card['title'];
+                            $subtitle = $card['description'];
+                            $bullets = $card['bullets'];
+                            $image = $card['image'];
+                            $button = $card['button'];
+                            $permalink = $button['url'] ?? '#';
+                            $target = !empty($button['target']) ? 'target="_blank" rel="noopener noreferrer"' : '';
+                            $label = $button['title'] ?? '';
+                            include(locate_template('components/card/card-05.php', false, false));
+                        endforeach; ?>
+                    <?php endif; ?>
                 </div>
-
             </div>
         </div>
     </div>
