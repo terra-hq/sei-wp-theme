b0c122796f0a87473e6deb223857b8914d808d4e
aresmiros <ares@terrahq.com>
2025-05-26 18:07:30 +0200

    Update form.php

--- flexible/modules/form.php
+++ flexible/modules/form.php
@@ -1,4 +1,4 @@
-<section class="c--hero-h <?php echo get_spacing($module['section_spacing']) ?>">
+<section id="form-hero" class="c--hero-h <?php echo get_spacing($module['section_spacing']) ?>">
     <div class="c--hero-h__bg-items"></div>
     <div class="c--hero-h__wrapper">
         <div class="f--row f--gap-c">
