6efe7f3dceba058ba5f01d10e4fd26f4f4814183
Nerea Fontecha <nerea@landt.co>
2025-05-27 14:52:23 +0200

    FIX Hero B

--- components/hero/hero-h.php
+++ components/hero/hero-h.php
@@ -1,18 +1,22 @@
-<section id="form-hero" class="c--hero-h">
+<section id="form-hero" class="c--hero-h <?= $customClass ?>">
     <div class="c--hero-h__bg-items"></div>
     <div class="c--hero-h__wrapper">
         <div class="f--row f--gap-c">
             <div class="f--col-6 f--col-tabletm-12">
                 <h1 class="c--hero-h__wrapper__title">
-                    <?php echo $hero_title ?>
+                    <?= $hero_title ?>
                 </h1>
-                <p class="c--hero-h__wrapper__subtitle">
-                    <?php echo $hero_subtitle ?>
-                </p>
-            </div>
-            <div class="f--col-5 f--col-tabletl-6 f--col-tabletm-12 f--offset-1 f--offset-tabletl-0">
-                <div class="c--form-d js--hubspot-script" data-form-id="<?php echo $hero_form_id ?>" data-portal-id="<?php echo $hero_form_portal ?>"></div>
-            </div>
+                <?php if($hero_subtitle && $hero_subtitle !== '') : ?>
+                    <p class="c--hero-h__wrapper__subtitle">
+                        <?= $hero_subtitle ?>
+                    </p>
+                <?php endif; ?>
+                </div>
+            <?php if($hero_form_id && $hero_form_portal) : ?>
+                <div class="f--col-5 f--col-tabletl-6 f--col-tabletm-12 f--offset-1 f--offset-tabletl-0">
+                    <div class="c--form-d js--hubspot-script" data-form-id="<?= $hero_form_id ?>" data-portal-id="<?= $hero_form_portal ?>"></div>
+                </div>
+            <?php endif; ?>
         </div>
     </div>
 </section>
