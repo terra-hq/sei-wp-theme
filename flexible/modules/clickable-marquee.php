46f9434f54838a700a9add569d0890d642a45689
felipeterrahq <felipe@terrahq.com>
2025-05-27 13:57:26 +0200

    feat: 🚀 add Scroll to form to Clickable Marquee

--- flexible/modules/clickable-marquee.php
+++ flexible/modules/clickable-marquee.php
@@ -1,5 +1,11 @@
-<section class="">
+<?php
+$section_spacing = get_spacing($module['section_spacing']);
+?>
+
+<section class=" <?= $section_spacing ?> ">
     <?php 
         include (locate_template('components/marquee/marquee-b.php'));
     ?>
 </section>
+
+<?php unset($section_spacing); ?>
