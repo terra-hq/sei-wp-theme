33bd3d6fafdb0d8f67740209f498aff713519897
Nerea Fontecha <nerea@landt.co>
2025-07-15 15:41:36 +0200

    Fix Include

--- functions/default/index.php
+++ functions/default/index.php
@@ -13,8 +13,8 @@ require get_template_directory() . '/functions/default/blocks/block-slider.php';
 require get_template_directory() . '/functions/default/blocks/block-stats.php'; 
 require get_template_directory() . '/functions/default/blocks/block-pills.php'; 
 require get_template_directory() . '/functions/default/blocks/block-icon-title.php'; 
-require get_template_directory() . '/functions/default/blocks/block-footnote.php'; 
-require get_template_directory() . '/functions/default/blocks/block-highlighted.php'; 
+require get_template_directory() . '/functions/default/blocks/global/block-footnote.php'; 
+require get_template_directory() . '/functions/default/blocks/global/block-highlighted.php'; 
 
 require get_template_directory() . '/functions/default/cleanHouse.php'; 
 require get_template_directory() . '/functions/default/terraClasses/index.php'; 
