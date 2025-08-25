d2b34aa1c74e2ea803c67714a9563514316463ca
Nerea Fontecha <nerea@landt.co>
2025-08-25 14:34:06 +0200

    Button

--- flexible/modules/button.php
+++ flexible/modules/button.php
@@ -1,17 +1,17 @@
 <?php
 $spacing = get_spacing($module['spacing']);
+$background_color = $module['background_color']
 $position_class = match ($module['button_position']) {
     'left'   => 'u--justify-content-start',
     'right'  => 'u--justify-content-flex-end',
     default  => 'u--justify-content-center'
 };
-
 $scroll_to_form = $module['scroll_to_form'];
 $scroll_to_form_title = $module['scroll_to_form_title'];
 $button = $module['button'];
 ?>
 
-<section class="<?= $spacing ?>">
+<section class="<?= $spacing ?> <?= $background_color ?>">
     <div class="f--container">
         <div class="f--row">
             <div class="f--col-12 u--display-flex <?= $position_class; ?>">
