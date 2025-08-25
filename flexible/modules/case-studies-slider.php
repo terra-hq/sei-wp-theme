47144307121ad0f9cb575770a5d75a9cf6be35b0
Nerea Fontecha <nerea@landt.co>
2025-08-25 14:19:53 +0200

    remove slider

--- flexible/modules/case-studies-slider.php
+++ flexible/modules/case-studies-slider.php
@@ -58,37 +58,6 @@ $case_studies = $module['case_studies'];
                             </div>
                         <?php } ?>
                     <?php } ?>
-                    <div class="c--slider-a__wrapper__item">
-                        <div class="c--card-m">
-                            <div class="f--row u--display-flex u--align-items-center">
-                                <div class="f--col-4 f--col-tabletm-12">
-                                    <div class="c--card-m__wrapper">
-                                        <img class="c--card-m__wrapper__media" src="http://placeholder.terrahq.com/img-1by1.webp" alt="">
-                                    </div>
-                                </div>
-                                
-                                <div class="f--col-4 f--col-tabletm-12">
-                                    <div class="c--card-m__hd">
-                                        <p class="c--card-m__hd__title">WE ARE SEI</p>
-                                        <p class="c--card-m__hd__paragraph">SEI is a management consulting firm delivering fresh perspectives and reliable results.</p>
-                                        <a class="g--link-01 g--link-01--fourth" href="">Learn More</a>
-                                    </div>
-                                </div>
-                                <div class="f--col-4 f--col-tabletm-12">
-                                    <div class="c--card-m__ft">
-                                        <p class="c--card-m__ft__title">WE ARE SEI</p>
-                                        <div class="c--card-m__ft__items">
-                                            <a class="g--pill-01" href="">Change Management</a>
-                                            <a class="g--pill-01" href="">Management</a>
-                                            <a class="g--pill-01" href="">Management Management</a>
-                                            <a class="g--pill-01" href="">Change Management</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>  
-                        </div>   
-                                    
-                    </div>
                 </div>
             </div>
         </div>
