1e0a8207154467988b071b3769ae9c227d833aaf
Ares Miró <ares@terrahq.com>
2025-09-16 14:16:11 +0200

    fix: 🐛 card-m component

--- components/card/card-m.php
+++ components/card/card-m.php
@@ -1,56 +1,56 @@
-<div class="c--card-m">
-    <div class="c--card-m__wrapper">
-        <?php
-            $image_tag_args = array(
-                'image' => get_field('picture', $person->ID),
-                'sizes' => 'medium',
-                'class' => 'c--card-m__wrapper__media',
-                'isLazy' => true,
-                'lazyClass' => 'g--lazy-01',
-                'showAspectRatio' => false,
-                'decodingAsync' => true,
-                'fetchPriority' => false,
-                'addFigcaption' => false,
-            );
-            if(get_field('picture', $person->ID)){
-                generate_image_tag($image_tag_args);
-            }
-            ?>
-        <h3 class="c--card-m__wrapper__title">
-            <?php echo get_the_title($person->ID) ?>
-        </h3>
-        <div class="c--card-m__wrapper__subtitle">
-            <?php $job_position = get_field('job_position', $person->ID);
-            if (!empty($job_position)) {
-                echo $job_position . ',';
-            } ?>
-
-            <?php
-            $location = get_field('location', $person->ID);
-            $post_title = '';
-            $post_url = '';
-
-            if (!empty($location) && is_array($location) && isset($location[0]) && is_object($location[0]) && property_exists($location[0], 'ID')) {
-                $post_title = $location[0]->post_title;
-                $post_url = get_permalink($location[0]->ID);
-            }
-            ?>
-            <a href="<?php echo $post_url ? $post_url : '#' ?>" class="c--card-m__wrapper__subtitle__link">
-                <?php echo $post_title ? $post_title : '' ?>
-            </a>
+ <div class="c--card-m">
+    <div class="f--row u--display-flex u--align-items-center">
+        <div class="f--col-4 f--col-tabletm-12">
+            <div class="c--card-m__wrapper">
+                <?php $image = get_post_thumbnail_id($single_case_study->ID); ?>
+                <?php
+                    $image_tag_args = array(
+                        'image' => $image,
+                        'sizes' => '(max-width: 810px) 50vw, 100vw',
+                        'class' => 'c--card-m__wrapper__media',
+                        'isLazy' => false,
+                        'showAspectRatio' => true,
+                        'decodingAsync' => true,
+                        'fetchPriority' => false,
+                        'addFigcaption' => false,
+                    );
+                    generate_image_tag($image_tag_args)
+                    ?>
+            </div>
         </div>
-        <div class="c--card-m__wrapper__media-wrapper">
-            <?php if(get_field('linkedin_link', $person->ID)): ?>
-            <a href="<?php echo get_field('linkedin_link', $person->ID) ?>" target="_blank" rel="noopener nofollow" class="c--card-m__wrapper__media-wrapper__link">
-                <img src="<?php bloginfo('template_url'); ?>/img/linkedin.svg" class="c--card-m__wrapper__media-wrapper__link__icon">
-            </a>
-            <?php endif; ?>
-            <?php if(get_field('email', $person->ID)): ?>
-            <a href="mailto:<?php echo get_field('email', $person->ID) ?>" class="c--card-m__wrapper__media-wrapper__link">
-                <img src="<?php bloginfo('template_url'); ?>/img/envelope.svg" class="c--card-m__wrapper__media-wrapper__link__icon">
-            </a>
-            <?php endif; ?>
-
+        
+        <?php 
+            $is_external = get_field('case_study_type', $single_case_study->ID) === "external";
+            if($is_external) {
+                $featured_case_study_link =  get_field('download_pdf', $single_case_study->ID);
+                $target= '_blank';
+                $rel = 'noopener noreferrer';
+            } else {
+                $featured_case_study_link = get_permalink($single_case_study->ID);
+                $target = '';
+                $rel = '';
+            }
+        ?>
+        <div class="f--col-4 f--col-tabletm-12">
+            <div class="c--card-m__hd">
+                <p class="c--card-m__hd__title">THE STARTING POINT</p>
+                <p class="c--card-m__hd__paragraph"><?= get_the_title($single_case_study->ID); ?></p>
+                <a class="g--link-01 g--link-01--fourth" href="<?= $featured_case_study_link ?>" target="<?= $target ?>"  rel="<?= $self ?>">Learn More</a>
+            </div>
         </div>
-    </div>
-</div>
+
+        <?php $types = get_the_terms($single_case_study->ID, 'case-study-capability'); ?>
+        <?php if($types)  { ?>
+                <div class="f--col-4 f--col-tabletm-12">
+                <div class="c--card-m__ft">
+                    <p class="c--card-m__ft__title">HOW WE HELPED</p>
+                    <div class="c--card-m__ft__items">
+                        <?php foreach ($types as $type) { ?>
+                            <a class="g--pill-01" href="<?= get_term_link($type) ?>"><?= $type->name ?></a>
+                        <?php } ?>
+                    </div>
+                </div>
+                </div>
+        <?php } ?>
+    </div>  
+</div>   
