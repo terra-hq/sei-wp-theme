0032db58d1576bbcb3e595bbb3a9ff307abf735e
Gotzon Alava <gotzon@terrahq.com>
2025-11-27 12:26:51 +0100

    feat: 🚀 create layout-01 for Center Heading + Text module

---
+++ components/layout/layout-01.php
@@ -0,0 +1,16 @@
+<!-- Se debe poder elegir color de fondo para añadir --second o no. -->
+
+<section class="g--layout-01">
+    <div class="g--layout-01__wrapper">
+        <div class="g--layout-01__wrapper__content">
+            <h2 class="g--layout-01__wrapper__content__item-primary">
+                SEI Takes An Outcomes-First Approach to AI Adoption
+            </h2>
+            <div class="g--layout-01__wrapper__content__list-group">
+                <p class="g--layout-01__wrapper__content__list-group__item">
+                    We bring trusted expertise and a pragmatic approach that ensures your AI strategy is clear, responsible, and built to last.
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
