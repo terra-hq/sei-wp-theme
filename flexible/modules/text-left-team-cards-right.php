b3ce710a37327d358ce711b6cb748168c3018aaf
Gotzon Alava <gotzon@terrahq.com>
2025-12-01 11:31:23 +0100

    fix: 🐛 text-left-team-cards-right module, cleanup duplicated module

--- flexible/modules/text-left-team-cards-right.php
+++ flexible/modules/text-left-team-cards-right.php
@@ -5,57 +5,64 @@ $subtitle = $module['subtitle'] ?? '';
 $experts = $module['experts'] ?? [];
 ?>
 
-<section class="<?= $spacing; ?>">
+<section class="c--layout-e <?= $spacing; ?>">
     <div class="f--container">
-        <div class="f--row">
-            <div class="f--col-12">
-
+        <div class="f--row f--gap-c u--justify-content-space-between">
+            <div class="f--col-5 f--col-tabletm-10 f--col-mobile-12">
                 <?php if ($title): ?>
-                    <p><strong>Title:</strong> <?= $title ?></p>
+                    <h2 class="c--layout-e__title">
+                        <?= $title ?>
+                    </h2>
                 <?php endif; ?>
-
                 <?php if ($subtitle): ?>
-                    <p><strong>Subtitle:</strong> <?= $subtitle ?></p>
+                    <div class="c--layout-e__content">
+                        <?= $subtitle ?>
+                    </div>
                 <?php endif; ?>
-
+            </div>
+            <div class="f--col-6 f--col-tabletm-10 f--col-mobile-12">
                 <?php if (!empty($experts)): ?>
-                    <?php foreach ($experts as $expert): ?>
-                        <div>
-                            <?php if (!empty($expert->picture)): ?>
-                                <p><strong>Picture:</strong></p>
-                                <?php
-                                $image_tag_args = [
-                                    'image' => $expert->picture,
-                                    'sizes' => 'medium',
-                                    'isLazy' => false
-                                ];
-                                generate_image_tag($image_tag_args);
-                                ?>
-                            <?php endif; ?>
+                    <div class="f--row f--gap-c u--justify-content-flex-end">
+                        <?php foreach ($experts as $expert): ?>
+                            <div class="f--col-6 f--col-mobile-12">
+                                <div class="c--card-c">
+                                    <div class="c--card-c__wrapper">
+                                        <div class="c--card-c__wrapper__media-wrapper">
+                                            <?php if (!empty($expert->picture)): ?>
+                                                <?php
+                                                $image_tag_args = [
+                                                    'image' => $expert->picture,
+                                                    'sizes' => 'medium',
+                                                    'class' => 'c--card-c__wrapper__media-wrapper__media',
+                                                    'isLazy' => true,
+                                                    'lazyClass' => 'g--lazy-01',
+                                                    'showAspectRatio' => false,
+                                                    'decodingAsync' => true,
+                                                    'fetchPriority' => false,
+                                                    'addFigcaption' => false,
+                                                ];
+                                                generate_image_tag($image_tag_args);
+                                                ?>
+                                            <?php endif; ?>
+                                        </div>
 
-                            <?php if (!empty($expert->people_single_description)): ?>
-                                <p><strong>Description:</strong> <?= $expert->people_single_description ?></p>
-                            <?php endif; ?>
-
-                            <?php if (!empty($expert->people_single_years)): ?>
-                                <p><strong>Years:</strong> <?= $expert->people_single_years ?></p>
-                            <?php endif; ?>
-
-                            <?php
-                            $specialities = get_field('people_single_specialities', $expert->ID) ?: [];
-                            if (!empty($specialities)): ?>
-                                <p><strong>Specialities:</strong></p>
-                                <ul>
-                                    <?php foreach ($specialities as $speciality): ?>
-                                        <li><?= $speciality['speciality_pill_text'] ?></li>
-                                    <?php endforeach; ?>
-                                </ul>
-                            <?php endif; ?>
-                        </div>
-                        <hr>
-                    <?php endforeach; ?>
+                                        <h3 class="c--card-c__wrapper__title">
+                                            <?= get_the_title($expert->ID) ?>
+                                        </h3>
+                                        <?php if (!empty($expert->people_single_description)): ?>
+                                            <div class="c--card-c__wrapper__subtitle">
+                                                <?= $expert->people_single_description ?>
+                                            </div>
+                                        <?php endif; ?>
+                                        <?php if (!empty($expert->people_single_years)): ?>
+                                            <span class="g--pill-01"><?= $expert->people_single_years ?></span>
+                                        <?php endif; ?>
+                                    </div>
+                                </div>
+                            </div>
+                        <?php endforeach; ?>
+                    </div>
                 <?php endif; ?>
-
             </div>
         </div>
     </div>
