8f05dcabb3c3f5eaa8eaed22ab134795f24c05b9
Gotzon Alava <gotzon@terrahq.com>
2025-12-01 11:12:09 +0100

    feat: 🚀 center heading with text variants

--- flexible/modules/center-heading-with-text.php
+++ flexible/modules/center-heading-with-text.php
@@ -1,18 +1,65 @@
 <?php
-$spacing = get_spacing($module['section_spacing']);
+$spacing = get_spacing($module['section_spacing']) ?? 'u--pt-15 u--pb-15';
 $bg_color = $module['bg_color'];
 $heading = $module['heading'];
 $text = $module['text'];
 ?>
 
-<section class="<?= $spacing; ?> <?= $bg_color; ?>">
-    <div class="f--container">
-        <div class="f--row">
-            <div class="f--col-12">
+<section class="g--layout-01 <?= $spacing; ?>">
+    <div class="g--layout-01__wrapper">
+        <div class="g--layout-01__wrapper__content">
+            <h2 class="g--layout-01__wrapper__content__item-primary">
+                SEI Takes An Outcomes-First Approach to AI Adoption
+            </h2>
+            <div class="g--layout-01__wrapper__content__list-group">
+                <p class="g--layout-01__wrapper__content__list-group__item">
+                    We bring trusted expertise and a pragmatic approach that ensures your AI strategy is clear, responsible, and built to last.
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
 
-                <p><strong>heading:</strong> <?= $heading; ?></p>
-                <p><strong>text:</strong> <?= $text; ?></p>
-                
+<section class="g--layout-01 <?= $spacing; ?> g--layout-01--second">
+    <div class="g--layout-01__wrapper">
+        <div class="g--layout-01__wrapper__content">
+            <h2 class="g--layout-01__wrapper__content__item-primary">
+                SEI Takes An Outcomes-First Approach to AI Adoption
+            </h2>
+            <div class="g--layout-01__wrapper__content__list-group">
+                <p class="g--layout-01__wrapper__content__list-group__item">
+                    We bring trusted expertise and a pragmatic approach that ensures your AI strategy is clear, responsible, and built to last.
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="g--layout-01 <?= $spacing; ?> g--layout-01--third">
+    <div class="g--layout-01__wrapper">
+        <div class="g--layout-01__wrapper__content">
+            <h2 class="g--layout-01__wrapper__content__item-primary">
+                SEI Takes An Outcomes-First Approach to AI Adoption
+            </h2>
+            <div class="g--layout-01__wrapper__content__list-group">
+                <p class="g--layout-01__wrapper__content__list-group__item">
+                    We bring trusted expertise and a pragmatic approach that ensures your AI strategy is clear, responsible, and built to last.
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="g--layout-01 <?= $spacing; ?> g--layout-01--fourth">
+    <div class="g--layout-01__wrapper">
+        <div class="g--layout-01__wrapper__content">
+            <h2 class="g--layout-01__wrapper__content__item-primary">
+                SEI Takes An Outcomes-First Approach to AI Adoption
+            </h2>
+            <div class="g--layout-01__wrapper__content__list-group">
+                <p class="g--layout-01__wrapper__content__list-group__item">
+                    We bring trusted expertise and a pragmatic approach that ensures your AI strategy is clear, responsible, and built to last.
+                </p>
             </div>
         </div>
     </div>
