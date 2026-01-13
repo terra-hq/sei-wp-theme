00f1a4880aada991113f99d39ed9d15ac6db3fbc
mauro-amestoy <mauro@terrahq.com>
2026-01-13 14:44:36 +0100

    Feat:🚀 added slider option on cta-content-left-image-right

--- flexible/modules/cta-content-left-image-right.php
+++ flexible/modules/cta-content-left-image-right.php
@@ -5,79 +5,116 @@ $modifierClass = '';
 if ($bg_color === 'f--background-c' || $bg_color === 'f--background-d') {
     $modifierClass = 'g--card-34--second';
 }
-$title = $module['title'];
-$description = $module['description'];
-$btn = $module['button'];
-
-$type = $module['type'];
-$first_image = $module['first_image'];
-$second_image = $module['second_image'];
-$second_image_text = $module['second_image_text'];
 $should_be_a_slider = $module['should_be_a_slider'];
-$slider = $module['slider']
+$slider = $module['slider'];
+
+$items = [];
+
+if ($slider && !empty($module['cards_repeater'])) {
+    $items = $module['cards_repeater']; 
+} else {
+    $items[] = [
+        'title' => $module['title'],
+        'description' => $module['description'],
+        'button' => $module['button'],
+        'type' => $module['type'],
+        'first_image' => $module['first_image'],
+        'second_image' => $module['second_image'],
+        'second_image_text' => $module['second_image_text']
+    ];
+}
 ?>
 
-<section class="<?= $spacing ?> <?= $bg_color ?>">
+<?php if ($slider): ?>
+    <section class="c--slider-a">
+        <div class="<?= $spacing ?> <?= $bg_color ?>">
+<?php else: ?>
+    <section class="<?= $spacing ?> <?= $bg_color ?>">
+<?php endif; ?>
     <div class="f--container">
         <div class="f--row">
             <div class="f--col-12">
-                <div class="g--card-34 <?= $modifierClass ?>">
-                    <div class="g--card-34__wrapper">
-                        <h2 class="g--card-34__wrapper__item-primary"><?= $title ?></h2>
-                        <p class="g--card-34__wrapper__item-secondary"><?= $description ?></p>
-                        <div class="g--card-34__wrapper__list-group">
-                            <?php if ($btn): ?>
-                                <a href="<?= $btn['url'] ?>" <?= get_target_link($btn['target'], $btn['title']) ?> rel="noopener noreferrer" class="g--card-34__wrapper__list-group__item">
-                                    <?= $btn['title'] ?>
-                                </a>
-                            <?php endif; ?>
+                <?php if ($slider): ?>
+                    <div class="c--slider-a__wrapper js--slider-e">
+                <?php endif; ?>
+                <?php 
+                foreach ($items as $item): 
+                    $title = $item['title'];
+                    $description = $item['description'];
+                    $btn = $item['button'];
+                    $type = isset($item['type']) ? $item['type'] : '';
+                    $first_image = $item['first_image'];
+                    $second_image = isset($item['second_image']) ? $item['second_image'] : null;
+                    $second_image_text = isset($item['second_image_text']) ? $item['second_image_text'] : '';
+                ?>
+                    <div class="c--slider-a__wrapper__item">
+                        <div class="g--card-34 <?= $modifierClass ?>">
+                            <div class="g--card-34__wrapper">
+                                <h2 class="g--card-34__wrapper__item-primary"><?= $title ?></h2>
+                                <p class="g--card-34__wrapper__item-secondary"><?= $description ?></p>
+                                <div class="g--card-34__wrapper__list-group">
+                                    <?php if ($btn): ?>
+                                        <a href="<?= $btn['url'] ?>" <?= get_target_link($btn['target'], $btn['title']) ?> rel="noopener noreferrer" class="g--card-34__wrapper__list-group__item">
+                                            <?= $btn['title'] ?>
+                                        </a>
+                                    <?php endif; ?>
+                                </div>
+                            </div>
+                            <figure class="g--card-34__media-wrapper">
+                                <?php 
+                                if ($first_image) {
+                                    $first_image_tag_args = [
+                                        'image' => $first_image,
+                                        'sizes' => '(max-width: 810px) 50vw, 10vw',
+                                        'class' => 'g--card-34__media-wrapper__media',
+                                        'isLazy' => true,
+                                        'lazyClass' => 'g--lazy-01',
+                                        'showAspectRatio' => true,
+                                        'decodingAsync' => true,
+                                        'fetchPriority' => false,
+                                        'addFigcaption' => false,
+                                    ];
+                                    generate_image_tag($first_image_tag_args);
+                                }
+                                ?>
+
+                                <?php if ($type === 'image_text' && $second_image): ?>
+                                    <div class="g--card-34__media-wrapper__wrapper">
+                                        <?php 
+                                        $second_image_tag_args = [
+                                            'image' => $second_image,
+                                            'sizes' => '(max-width: 810px) 50vw, 10vw',
+                                            'class' => 'g--card-34__media-wrapper__wrapper__media',
+                                            'isLazy' => true,
+                                            'lazyClass' => 'g--lazy-01',
+                                            'showAspectRatio' => true,
+                                            'decodingAsync' => true,
+                                            'fetchPriority' => false,
+                                            'addFigcaption' => false,
+                                        ];
+                                        generate_image_tag($second_image_tag_args);
+                                        ?>
+                                        <p class="g--card-34__media-wrapper__wrapper__title"><?= $second_image_text ?></p>
+                                    </div>
+                                    <div class="g--card-34__media-wrapper__overlay"></div>
+                                <?php endif; ?>
+                            </figure>
                         </div>
                     </div>
-                    <figure class="g--card-34__media-wrapper">
-                        <?php 
-                        if ($first_image) {
-                            $first_image_tag_args = [
-                                'image' => $first_image,
-                                'sizes' => '(max-width: 810px) 50vw, 10vw',
-                                'class' => 'g--card-34__media-wrapper__media',
-                                'isLazy' => true,
-                                'lazyClass' => 'g--lazy-01',
-                                'showAspectRatio' => true,
-                                'decodingAsync' => true,
-                                'fetchPriority' => false,
-                                'addFigcaption' => false,
-                            ];
-                            generate_image_tag($first_image_tag_args);
-                        }
-                        ?>
-
-                        <?php if ($type === 'image_text' && $second_image): ?>
-                            <div class="g--card-34__media-wrapper__wrapper">
-                                <?php 
-                                $second_image_tag_args = [
-                                    'image' => $second_image,
-                                    'sizes' => '(max-width: 810px) 50vw, 10vw',
-                                    'class' => 'g--card-34__media-wrapper__wrapper__media',
-                                    'isLazy' => true,
-                                    'lazyClass' => 'g--lazy-01',
-                                    'showAspectRatio' => true,
-                                    'decodingAsync' => true,
-                                    'fetchPriority' => false,
-                                    'addFigcaption' => false,
-                                ];
-                                generate_image_tag($second_image_tag_args);
-                                ?>
-                                <p class="g--card-34__media-wrapper__wrapper__title"><?= $second_image_text ?></p>
-                            </div>
-                            <div class="g--card-34__media-wrapper__overlay"></div>
-                        <?php endif; ?>
-                    </figure>
-                </div>
+                <?php endforeach; ?>
+                <?php if ($slider): ?>
+                    </div>
+                <?php endif; ?>
             </div>
         </div>
     </div>
-</section>
+<?php if ($slider): ?>
+        </div>
+    </section>
+<?php else: ?>
+    </section>
+<?php endif; ?>
 
 <?php 
-unset($spacing, $bg_color, $modifierClass, $title, $description, $btn, $type, $first_image, $second_image, $second_image_text); 
+unset($spacing, $bg_color, $modifierClass, $items, $slider);
 ?>
