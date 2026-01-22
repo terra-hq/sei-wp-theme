39834f2fac337f45e3914456e5f773ecc2cc8eec
NereaFontecha <nerea@terrahq.com>
2026-01-22 12:20:20 +0100

    feat: 🚀 footer aI tools

---
+++ components/social/social-b.php
@@ -0,0 +1,25 @@
+<nav class="c--social-a <?= $socialCustomClass ?>">
+    <h3><?= $socialbTitle ?></h3>
+    <?php foreach ($socialbLinks as $key => $singleAI) { ?>
+        <?php 
+            $ia_image_url = $singleAI['url'];
+            $ia_host = parse_url($url, PHP_URL_HOST);      // chatgpt.com
+            $ia_name = preg_replace('/\..*/', '', $host);  
+        ?>
+         <a href="<?= $ia_image_url  ?>" target="_blank" rel="noopener noreferrer" class="" aria-label="<?= $ia_name ?>">
+            <?php if($singleAI['image']) : ?>
+            <?php  $image_tag_args = array(
+                'image' => $singleAI['image'],
+                'sizes' => 'small',
+                'class' => '',
+                'isLazy' => true,
+                'lazyClass' => 'g--lazy-01',
+                'showAspectRatio' => true,
+                'decodingAsync' => true,
+                'fetchPriority' => false,
+                'addFigcaption' => false,
+            );
+            generate_image_tag($image_tag_args) ; endif;  ?>
+        </a>
+    <?php } ?>
+</nav>
