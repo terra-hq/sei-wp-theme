3d2038c2dcee525802d9a93572f138458acff0ba
angelavilasanta <angela@terrahq.com>
2026-01-28 12:05:15 +0100

    Feat: 🚀 card-n

--- flexible/modules/cta-content-left-image-right.php
+++ flexible/modules/cta-content-left-image-right.php
@@ -45,58 +45,7 @@ if (!$should_be_a_slider) {
                     $second_image_text = isset($item['second_image_text']) ? $item['second_image_text'] : '';
                 ?>
                     <div class="c--slider-a__wrapper__item">
-                        <div class="g--card-34 <?= $modifierClass ?>">
-                            <div class="g--card-34__wrapper">
-                                <h2 class="g--card-34__wrapper__item-primary"><?= $title ?></h2>
-                                <p class="g--card-34__wrapper__item-secondary"><?= $description ?></p>
-                                <div class="g--card-34__wrapper__list-group">
-                                    <?php if ($btn): ?>
-                                        <a href="<?= $btn['url'] ?>" <?= get_target_link($btn['target'], $btn['title']) ?> rel="noopener noreferrer" class="g--card-34__wrapper__list-group__item">
-                                            <?= $btn['title'] ?>
-                                        </a>
-                                    <?php endif; ?>
-                                </div>
-                            </div>
-                            <figure class="g--card-34__media-wrapper">
-                                <?php 
-                                if ($first_image) {
-                                    $first_image_tag_args = [
-                                        'image' => $first_image,
-                                        'sizes' => '(max-width: 810px) 50vw, 10vw',
-                                        'class' => 'g--card-34__media-wrapper__media',
-                                        'isLazy' => true,
-                                        'lazyClass' => 'g--lazy-01',
-                                        'showAspectRatio' => true,
-                                        'decodingAsync' => true,
-                                        'fetchPriority' => false,
-                                        'addFigcaption' => false,
-                                    ];
-                                    generate_image_tag($first_image_tag_args);
-                                }
-                                ?>
-
-                                <?php if ($type === 'image_text' && $second_image): ?>
-                                    <div class="g--card-34__media-wrapper__wrapper">
-                                        <?php 
-                                        $second_image_tag_args = [
-                                            'image' => $second_image,
-                                            'sizes' => '(max-width: 810px) 50vw, 10vw',
-                                            'class' => 'g--card-34__media-wrapper__wrapper__media',
-                                            'isLazy' => true,
-                                            'lazyClass' => 'g--lazy-01',
-                                            'showAspectRatio' => true,
-                                            'decodingAsync' => true,
-                                            'fetchPriority' => false,
-                                            'addFigcaption' => false,
-                                        ];
-                                        generate_image_tag($second_image_tag_args);
-                                        ?>
-                                        <p class="g--card-34__media-wrapper__wrapper__title"><?= $second_image_text ?></p>
-                                    </div>
-                                    <div class="g--card-34__media-wrapper__overlay"></div>
-                                <?php endif; ?>
-                            </figure>
-                        </div>
+                        <?php include(locate_template('components/card/card-n.php', false, false)); ?>
                     </div>
                 <?php endforeach; ?>
                 <?php if ($should_be_a_slider): ?>
