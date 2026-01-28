9c3b01b93837fb7aa19c2c7a48c6b6c7281c42db
mauro-amestoy <mauro@terrahq.com>
2026-01-28 11:16:48 +0100

    Fix: 🐛 delete slider-e

--- src/js/handler/slider/slidersConfig.js
+++ src/js/handler/slider/slidersConfig.js
@@ -110,34 +110,4 @@ const sliderDConfig = {
   }
 };
 
-const sliderEConfig = {
-  loop: true,
-  items: 1,
-  gutter: 32,
-  slideBy: 1,
-  controls: false,
-  nav: true,
-  navPosition: "bottom",
-  rewind: false,
-  swipeAngle: 60,
-  lazyload: true,
-  lazyloadSelector: ".tns-lazy-img",
-  autoplay: true,
-  mouseDrag: true,
-  autoplayButtonOutput: false,
-  speed: 350,
-  autoplayTimeout: 40000,
-  preventActionWhenRunning: true,
-  preventScrollOnTouch: "auto",
-  touch: true,
-  responsive: {
-    0: {
-      disable: false,
-    },
-    811: {
-      disable: false,
-    }
-  }
-};
-
 export { sliderAConfig, sliderBConfig, sliderCConfig, sliderDConfig, sliderEConfig };
