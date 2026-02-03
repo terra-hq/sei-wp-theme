9b40be65342baef6a58580a30c64a3c8afe16594
NereaFontecha <nerea@terrahq.com>
2026-02-03 11:44:20 +0100

    feat: 🚀 Remove Boostify Licence and add worker

--- src/js/Project.js
+++ src/js/Project.js
@@ -34,7 +34,6 @@ class Project {
 
     this.boostify = new Boostify({
       debug: this.terraDebug,
-      // license: import.meta.env.VITE_LICENSE_KEY,
     });
 
     if (this.terraDebug) {
@@ -189,7 +188,6 @@ class Project {
                       virtualPageTitle: document.title, // Page title
                       virtualPagePath: window.location.pathname, // Path w/o hostname
                   });
-                  console.log("windowdatalayer", window.dataLayer);
               },
           });
       }
