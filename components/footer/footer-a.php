29270db8ca5089e8e1a15c499ca09e09a832c226
mauro-amestoy <mauro@terrahq.com>
2026-02-19 13:47:46 +0100

    Refactor: 🐛 Footer

--- components/footer/footer-a.php
+++ components/footer/footer-a.php
@@ -2,7 +2,7 @@
     <div class="f--container">
         <div class="c--footer-a__hd">
             <div class="f--row">
-                <div class="f--col-7 f--col-laptop-8 f--col-tabletl-12">
+                <div class="f--col-12 f--col-tabletl-8 f--col-laptop-7">
                     <?php $title = '';
                     $footer_titles = get_field('footer_title', 'option');
                     if ($footer_titles) {
@@ -20,7 +20,7 @@
                         <a href="<?php echo esc_url(get_field('footer_link', 'option')); ?>" class="c--footer-a__hd__link"><?php echo $title; ?></a>
                     <?php endif; ?>
                 </div>
-                <div class="f--col-5 f--col-laptop-4 f--col-tabletl-12">
+                <div class="f--col-12 f--col-tabletl-4 f--col-laptop-5">
                     <?php $socialbTitle = get_field('ask_ai_about_sei','option') ?>
                     <?php $socialbLinks = get_field('ai_links','option') ?>
                     <?php include(locate_template('components/social/social-b.php', false, false)); ?>
@@ -29,13 +29,13 @@
         </div>
         <div class="c--footer-a__bd">
             <div class="f--row f--gap-a u--justify-content-space-between">
-                <div class="f--col-7 f--col-laptop-6 f--col-tabletl-8 f--col-tabletl-12">
+                <div class="f--col-12 f--col-tabletm-10 f--col-tabletl-6 f--col-laptop-7">
                     <div class="c--footer-a__bd__item c--form-a js--hubspot-script--footer" data-form-id=<?php echo get_field('form_id', 'option') ?> data-portal-id=<?php echo get_field('form_portal_id', 'option') ?>>
                     </div>
                 </div>
                 <?php $navigation_links = get_field('navigation_links', 'option');
                 if ($navigation_links) : ?>
-                    <div class="f--col-5 f--col-tabletl-12">
+                    <div class="f--col-12 f--col-tabletl-5">
                         <nav class="c--nav-c">
                             <?php foreach ($navigation_links as $navEach) : ?>
                                 <a href="<?php echo $navEach['link']['url'] ?>" <?= get_target_link($navEach['link']['target'], $navEach['link']['title'] )?> class="c--nav-c__item"><?php echo $navEach['link']['title'] ?></a>
@@ -48,7 +48,7 @@
         <div class="c--footer-a__ft">
             <div class="c--footer-a__ft__wrapper">
                 <div class="f--row f--gap-c">
-                    <div class="f--col-9 f--col-laptop-8 f--col-tabletl-12">
+                    <div class="f--col-12 f--col-tabletl-8 f--col-laptop-9">
                         <?php $extra_links = get_field('extra_links', 'option');
                         if ($extra_links) : ?>
                             <nav class="c--footer-a__ft__wrapper__list-group c--nav-d">
@@ -59,7 +59,7 @@
                         <?php endif; ?>
                         <p class="f--font-j f--color-a">Copyright © <?php echo date('Y') ?> <?php echo get_field('copyright', 'option') ?></p>
                     </div>
-                    <div class="f--col-3 f--col-laptop-4 f--col-tabletl-12">
+                    <div class="f--col-12 f--col-tabletl-4 f--col-laptop-3">
                         <?php $socialCustomClass = 'c--footer-a__ft__wrapper__item-right' ?>
                         <?php include(locate_template('components/social/social-a.php', false, false)); ?>
                     </div>
